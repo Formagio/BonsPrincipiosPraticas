eeb093482a1a06cbf5ad41825b20c64d745e25b4
Róger Formagio Lopes <rogerformagiolopes@hotmail.com>
2019-05-12 20:53:13 -0300

    GRASP - Variações protegidas

---
+++ BonsPrincipiosPraticas/GRASP/VariacoesProtegidas/MovimentacaoComVP.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BonsPrincipiosPraticas.GRASP.VariacoesProtegidas.MovimentacaoComVP
+{
+    public enum TipoMovimentacaoFinanceira
+    {
+        CONTA_PAGAR = 1,
+        CONTA_RECEBER = 2,
+        COBRANCA = 3
+    }
+
+    public interface IMovimentacaoFinanceira
+    {
+        void GerarMovimentacaoFinanceira(double valor);
+    }
+
+    public class ContaPagar : IMovimentacaoFinanceira
+    {
+        public void GerarMovimentacaoFinanceira(double valor)
+        {
+        }
+    }
+
+    public class ContaReceber : IMovimentacaoFinanceira
+    {
+        public void GerarMovimentacaoFinanceira(double valor)
+        {
+        }
+    }
+
+    public class Cobranca : IMovimentacaoFinanceira
+    {
+        public void GerarMovimentacaoFinanceira(double valor)
+        {
+        }
+    }
+
+    public static class MovimentacaoFinanceiraFactory
+    {
+        public static IMovimentacaoFinanceira CriarObjeto(TipoMovimentacaoFinanceira tipo)
+        {
+            switch (tipo)
+            {
+                case TipoMovimentacaoFinanceira.CONTA_PAGAR:
+                    return new ContaPagar();
+                case TipoMovimentacaoFinanceira.CONTA_RECEBER:
+                    return new ContaReceber();
+                case TipoMovimentacaoFinanceira.COBRANCA:
+                    return new Cobranca();
+                default:
+                    throw new Exception("Tipo de operação financeira não implementado");
+            }
+        }
+    }
+
+    public class Financeiro
+    {
+        private void GerarMovimentacaoFinanceira(double valor, TipoMovimentacaoFinanceira tipo)
+        {
+            MovimentacaoFinanceiraFactory.CriarObjeto(tipo).GerarMovimentacaoFinanceira(valor);
+        }
+    }
+}
---
+++ BonsPrincipiosPraticas/GRASP/VariacoesProtegidas/MovimentacaoSemVP.cs
@@ -0,0 +1,49 @@
+﻿namespace BonsPrincipiosPraticas.GRASP.VariacoesProtegidas.MovimentacaoSemVP
+{
+    public enum TipoMovimentacaoFinanceira
+    {
+        CONTA_PAGAR = 1,
+        CONTA_RECEBER = 2,
+        COBRANCA = 3
+    }
+
+    public class ContaPagar
+    {
+        public void GerarContaPagar(double valor)
+        {
+        }
+    }
+
+    public class ContaReceber
+    {
+        public void GerarContaReceber(double valor)
+        {
+        }
+    }
+
+    public class Cobranca
+    {
+        public void GerarCobranca(double valor)
+        {
+        }
+    }
+
+    public class Financeiro
+    {
+        private void GerarMovimentacaoFinanceira(double valor, TipoMovimentacaoFinanceira tipo)
+        {
+            switch (tipo)
+            {
+                case TipoMovimentacaoFinanceira.CONTA_PAGAR:
+                    new ContaPagar().GerarContaPagar(valor);
+                    break;
+                case TipoMovimentacaoFinanceira.CONTA_RECEBER:
+                    new ContaReceber().GerarContaReceber(valor);
+                    break;
+                case TipoMovimentacaoFinanceira.COBRANCA:
+                    new Cobranca().GerarCobranca(valor);
+                    break;
+            }
+        }
+    }
+}
